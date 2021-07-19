c8a728997bcec6ac5edfcd6cc9b89ec26cbfb258
Tomaž Ovsenjak <tomazovsenjak@Tomazs-MacBook-Air.fritz.box>
2021-07-19 15:13:24 +0200

    finding the latest job

--- webScrapeSloTech.js
+++ webScrapeSloTech.js
@@ -6,21 +6,30 @@ const puppeteer = require('puppeteer');
    
 //get 100 jbos from the slotech web page 
 
-const jobs = [
+//get the latest delo
+await page.goto(`https://slo-tech.com/delo`);
 
-];
+let TextPage = [];
+for (let i = 1; i < 10; i++) {
+  let [getTextIconXPath] = await page.$x(`//*[@id="content"]/table/tbody/tr[${i}]/td[2]/h3/a`);
+  const getTextIcon = await page.evaluate(name => name.href, getTextIconXPath);
+  let numberOfJob =getTextIcon.split("/")[4]
+  TextPage.push(Number(numberOfJob))
+}
+let jobNumberToStartWith = Math.max(...TextPage)
+let jobs = [];
 
-
-for (var i =100- 1; i >= 0; i--) {
+for (var i =10- 1; i >= 0; i--) {
     //current job number
-    let delo = 4178
+    let delo = jobNumberToStartWith
     let job = {
       title:"",
-      numberOfJob:"",
+      numberOfJob:delo-i,
       employer:"",
       dateFrom:"",
       placilo:"",
-      lokacija:""
+      lokacija:"",
+      zahteve:""
     };
     await page.goto(`https://slo-tech.com/delo/${delo-i}`);
 
@@ -30,24 +39,42 @@ for (var i =100- 1; i >= 0; i--) {
     let [employerXpath] = await page.$x("//*[@id='content']/h3/a");
     let [placiloXpath] = await page.$x("//*[@id='content']/dl[2]/dd");
     let [lokacijaXPath] = await page.$x("//*[@id='content']/dl[1]/dd/a");
+    let [zahteveXpath] = await page.$x("//*[@id='content']/p[2]");
     //evaluating
-    const datePosted = await page.evaluate(name => name.textContent, dateOfPostingXPath);
+    let datePosted = await page.evaluate(name => name.textContent, dateOfPostingXPath);
     const fullTitle = await page.evaluate(name => name.textContent, titleXPath);
     const placilo = await page.evaluate(name => name.textContent, placiloXpath);
     const lokacija = await page.evaluate(name => name.textContent, lokacijaXPath);
+    const zahteve = await page.evaluate(name => name.textContent, zahteveXpath);
     
     
     const title = fullTitle.split("@")[0]
+    if(title.includes("brisan")){
+      continue;
+    }
     const employer = fullTitle.split("@")[1]
-    
+    if (datePosted.includes("danes")){
+      console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+      const date = new Date();
+      const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
+      console.log(date)
+      console.log(month,day,year)
+      const mothnArray = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"]
+      let dateYposted = datePosted.replace("danes",`${day}. ${mothnArray[month]} ${year} `)
+      job.dateFrom = dateYposted;
+    }else {
+      job.dateFrom = datePosted
+    }
     //const employer = await page.evaluate(name => name.textContent, employerXpath);
-    job.dateFrom = datePosted;
+  
     job.title = title;
     job.employer = employer;
     job.placilo = placilo;
     job.lokacija= lokacija;
+    job.zahteve = zahteve;
     jobs.push(job);
   }
   console.log(jobs)
+  console.log(jobs.length)
   await browser.close();
 })();
