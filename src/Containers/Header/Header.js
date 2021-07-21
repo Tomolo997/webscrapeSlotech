410e2364fe805c9fe08fb97c6c0b46644a998e23
Tomaž Ovsenjak <tomazovsenjak@Tomazs-Air.fritz.box>
2021-07-21 18:55:00 +0200

    added the header and search div

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -4,10 +4,19 @@ export default function Header() {
   return (
     <div className={styles.MainDiv}>
       <div className={styles.mainNav}>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div className={styles.PostJobDiv}>post a job</div>
+        <div className={styles.mainHeadingDiv}>
+          <h1 className={styles.mainHeadingH1}>
+            No bullshit Slovenian IT jobs
+          </h1>
+          <h1 className={styles.mainHeadingH2}>
+            Salary, Programming language and employment level
+            <br></br>
+            <br></br> Filter the bullshit!
+          </h1>
+          <div className={styles.mainHeadingSearchDiv}>
+            <input className={styles.searchInputJob} />
+          </div>
+        </div>
       </div>
     </div>
   );
