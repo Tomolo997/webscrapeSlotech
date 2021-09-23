f1f75be6ae5d5854cb8b2770a8271cf613e87c08
Tomaž Ovsenjak <tomazovsenjak@Tomazs-Air.fritz.box>
2021-09-23 11:36:55 +0200

    new server

--- server/server.js
+++ server/server.js
@@ -4,14 +4,25 @@ const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Jobs = require("./JobsModel");
+const JobsCopy = require("./jobsModelCopy");
 const app = express();
 const port = process.env.PORT || 4001;
 //connection to the DB
+
 function compare(a, b) {
   if (a.maximumPlacilo < b.maximumPlacilo) {
     return 1;
   }
   if (a.maximumPlacilo > b.maximumPlacilo) {
+    return -1;
+  }
+  return 0;
+}
+function ComparenumberOfJob(a, b) {
+  if (a.numberOfJob < b.numberOfJob) {
+    return 1;
+  }
+  if (a.numberOfJob > b.numberOfJob) {
     return -1;
   }
   return 0;
@@ -33,12 +44,14 @@ mongoose
     console.log("DB collection succesful");
   });
 app.use(cors());
+app.use(express.json());
 
 app.listen(port, (_) => console.log(`The server is listening on port ${port}`));
 
 app.get("/api/v1/jobs", async (req, res) => {
   try {
-    const jobs = await Jobs.find();
+    const jobs = await JobsCopy.find();
+    jobs.sort(ComparenumberOfJob);
     res.status(200).json({
       status: "success",
       jobs: jobs,
@@ -53,6 +66,35 @@ app.get("/api/v1/jobs-sorted-by-pay", async (req, res) => {
     res.status(200).json({
       status: "success",
       jobs: sortbyPlacilo(jobs),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const sortByLanguage = async (array) => {
+  let sendJobs = [];
+
+  let jobs = await Jobs.aggregate([
+    {
+      $match: {
+        programmingLanguages: {
+          $in: array,
+        },
+      },
+    },
+  ]);
+
+  return jobs;
+};
+
+app.get("/api/v1/sort/:text", async (req, res) => {
+  try {
+    console.log(req.params.text);
+    let arrayOfLang = req.params.text.split("&");
+    res.status(200).json({
+      status: "success",
+      jobs: await sortByLanguage(arrayOfLang),
     });
   } catch (error) {
     console.log(error);
